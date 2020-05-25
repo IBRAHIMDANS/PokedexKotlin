c24400eb04393b6170c374c9eb63440d34e724d9
Benjamin Benoit <>
2020-05-25 15:31:12 +0200

    viewmodel

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.pokedex
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.pokedex.network.Api
@@ -14,16 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     private val pokemonWebService = Api.pokemonService
 
+
+    private val viewModelTask by lazy {
+        ViewModelProvider(this).get(PokemonViewModel::class.java)
+    }
+
     //Liste des pokemons
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pokemons_list)
         val glide = Glide.with(this)
         lifecycleScope.launch {
             val pokemonInfo = getInfo()
         }
     }
-
 
     suspend fun getInfo(): List<PokemonInfo?>? {
         val tasksResponse = pokemonWebService.getAll()
--- app/src/main/java/com/example/pokedex/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/PokemonViewModel.kt
@@ -10,6 +10,19 @@ class PokemonViewModel : ViewModel() {
     private val pokemonInfoRepository: PokemonInfoRepository = PokemonInfoRepository()
     private val _pokemonInfo = MutableLiveData<List<PokemonInfo>>()
     public val pokemonInfo: LiveData<List<PokemonInfo>> = _pokemonInfo
+    private val _pokemonList = MutableLiveData<List<PokemonInfo>>()
+
+    private fun getMutableList() = _pokemonList.value.orEmpty().toMutableList()
+
+    fun loadPokemons() {
+        viewModelScope.launch {
+            val newList = pokemonInfoRepository.getPokemonList()
+            _pokemonList.value = getMutableList().apply {
+                clear()
+                newList?.let { addAll(it) }
+            }
+        }
+    }
 
     fun getPokemon(pokemonInfo: PokemonInfo) {
         viewModelScope.launch {
